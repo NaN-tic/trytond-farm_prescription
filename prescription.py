1662d2a413f10a7bbeffe97130bea03f7fb1f857
Sergi Carol <sergi@nan-tic.com>
2017-06-07 14:40:57 +0200

    Removed domain that forcer the internal shipment to be from the same farm

--- prescription.py
+++ prescription.py
@@ -788,7 +788,6 @@ class CreateInternalShipmentStart(ModelView):
         ])
     from_location = fields.Many2One('stock.location', 'From Location',
         domain=[
-            ('warehouse', '=', Eval('farm')),
             ('type', '=', 'storage'),
             ('silo', '=', False),
             ], depends=['farm'], required=True)
